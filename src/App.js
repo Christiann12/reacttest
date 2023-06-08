2772e7c6ad102b0e96c50e9c35645d3dd81e9c30
Christiann12 <christianpaulpili@yahoo.com>
2023-06-08 18:35:45 +0800

    react list and list map function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './Index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
-
+import { v4 as uuidv4 } from 'uuid';
 function App() {
   const [role, setRole] = useState("dev");
+  const [employees, setEmployees] = useState(
+    [
+      {name: "Christian", role: "Intern", src: "https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"},
+      {name: "Christian", role: "Intern", src: "https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"},
+      {name: "Christian", role: "Intern", src: "https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"},
+      {name: "Christian", role: "Intern", src: "https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"},
+    ]
+  );
   const showEmployees = true;
   return (
     <div className="App">
@@ -16,12 +24,16 @@ function App() {
             }}
           />
           <div className='flex flex-wrap justify-center'>
-            <Employee name="Christian" role="Intern" src="https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"/>
-            <Employee name="Paul" role="Intern" src="https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"/>
-            <Employee name="Christian" role="Intern" src="https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"/>
-            <Employee name="Paul" role="Intern" src="https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"/>
-            <Employee name="Christian" role="Intern" src="https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"/>
-            <Employee name="Paul" role="Intern" src="https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"/>
+            {employees.map((employee) => { 
+              return (
+                <Employee
+                    key={uuidv4()}
+                    name={employee.name}
+                    role={employee.role}
+                    src={employee.src}
+                  />
+              );
+            })}
           </div>
         </>
       )
