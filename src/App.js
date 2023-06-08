0a07174bcc7ca86e57a6d81bcc1fc2a0f325787c
Christiann12 <christianpaulpili@yahoo.com>
2023-06-08 17:47:56 +0800

    react hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
+  const [role, setRole] = useState("dev");
   const showEmployees = true;
   return (
     <div className="App">
       {showEmployees ? (
         <>
+          <input
+            type='text'
+            onChange={(e) => {
+              setRole(e.target.value)
+            }}
+          />
           <Employee name="Christian" role="Intern"/>
-          <Employee name="Paul"/>
+          <Employee name="Paul" role={role} />
         </>
       )
       :(
