6adf20fbfb50e60ddbc93ae815d4345f40189caf
Christiann12 <christianpaulpili@yahoo.com>
2023-06-08 23:01:36 +0800

    Routing and header styling

--- src/App.js
+++ src/App.js
@@ -1,79 +1,20 @@
-import './Index.css';
-import Employee from './components/Employee';
-import AddEmployee from './components/AddEmployee';
-import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import EditEmployee from './components/EditEmployee';
+import "./Index.css";
+import Customer from "./Pages/Customers";
+import Employees from "./Pages/Employees";
+import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  const [role, setRole] = useState("dev");
-  const [employees, setEmployees] = useState(
-    [
-      {id:1 ,name: "Christian", role: "Intern", src: "https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"},
-      {id:2 ,name: "Christian", role: "Intern", src: "https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"},
-      {id:3 ,name: "Christian", role: "Intern", src: "https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"},
-      {id:4 ,name: "Christian", role: "Intern", src: "https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"},
-      {id:5 ,name: "Christian", role: "Intern", src: "https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"},
-      {id:6 ,name: "Christian", role: "Intern", src: "https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"},
-      {id:7 ,name: "Christian", role: "Intern", src: "https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"},
-      {id:8 ,name: "Christian", role: "Intern", src: "https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"},
-    ]
-  );
-
-  function updateEmployee(id, newName, newRole) {
-    const updatedEmployees = employees.map((employee) => {
-      if (id == employee.id) {
-        return { ...employee, name: newName, role: newRole };
-      } 
-
-      return employee;
-    });
-    setEmployees(updatedEmployees);
-  }
-  function newEmployee(newName, newRole, newImage) {
-    const newEmployeeData = {
-      id: uuidv4(),
-      name: newName,
-      role: newRole,
-      src: newImage
-    };
-    setEmployees([...employees, newEmployeeData]);
-    console.log(newEmployeeData);
-  }
-  const showEmployees = true;
-  return (
-    <div className="App">
-      {showEmployees ? (
-        <>
-          <div className='flex flex-wrap justify-center'>
-            {employees.map((employee) => { 
-              const editEmployee = <EditEmployee
-                id={employee.id}
-                name={employee.name}
-                role={employee.role}
-                updateEmployee = {updateEmployee}
-              />
-              return (
-                <Employee
-                  key={uuidv4()}
-                  name={employee.name}
-                  role={employee.role}
-                  src={employee.src}
-                  id={employee.id}
-                  editEmployee = {editEmployee}
-                />
-              );
-            })}
-          </div>
-          <AddEmployee newEmployee = {newEmployee} />
-        </>
-      )
-      :(
-        <p>you cannot see the employees</p>
-      )
-
-      }
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Header>
+				<Routes>
+					<Route path="/Employees" element={<Employees />} />
+					<Route path="/Customers" element={<Customer />} />
+				</Routes>
+			</Header>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,25 +1,27 @@
-
 import EditEmployee from "./EditEmployee";
 
 function Employee(props) {
-    return (
-        <div className="min-w-[350px] max-w-[350px] my-2 mx-3 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-            <img className="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.src} alt="Woman's Face" />
-            <div className="text-center space-y-2 sm:text-left"> 
-            <div className="space-y-0.5">
-                <p className="text-lg text-black font-semibold">
-                    {props.name ? props.name : "No Name"}
-                </p>
-                <p className="text-slate-500 font-medium">
-                    {props.role ? props.role : "No Role"} 
-                </p>
-                </div>
-                
-                {props.editEmployee}
-            </div>
-        </div>
-               
-    )
+	return (
+		<div className="min-w-[350px] max-w-[350px] my-2 mx-3 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+			<img
+				className="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+				src={props.src}
+				alt="Woman's Face"
+			/>
+			<div className="text-center space-y-2 sm:text-left">
+				<div className="space-y-0.5">
+					<p className="text-lg text-black font-semibold">
+						{props.name ? props.name : "No Name"}
+					</p>
+					<p className="text-slate-500 font-medium">
+						{props.role ? props.role : "No Role"}
+					</p>
+				</div>
+
+				{props.editEmployee}
+			</div>
+		</div>
+	);
 }
 
 export default Employee;
