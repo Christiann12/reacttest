24d0de32bcd9b88234ea9d7f7aac0fc8ab49e954
Christiann12 <christianpaulpili@yahoo.com>
2023-06-08 18:21:17 +0800

    created cards using tailwind css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [role, setRole] = useState("dev");
   const showEmployees = true;
   return (
-    <div className="App bg-red-300">
+    <div className="App">
       {showEmployees ? (
         <>
           <input
@@ -15,8 +15,14 @@ function App() {
               setRole(e.target.value)
             }}
           />
-          <Employee name="Christian" role="Intern"/>
-          <Employee name="Paul" role={role} />
+          <div className='flex flex-wrap justify-center'>
+            <Employee name="Christian" role="Intern" src="https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"/>
+            <Employee name="Paul" role="Intern" src="https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"/>
+            <Employee name="Christian" role="Intern" src="https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"/>
+            <Employee name="Paul" role="Intern" src="https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"/>
+            <Employee name="Christian" role="Intern" src="https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"/>
+            <Employee name="Paul" role="Intern" src="https://images.pexels.com/photos/17016912/pexels-photo-17016912/free-photo-of-canelo.jpeg"/>
+          </div>
         </>
       )
       :(
